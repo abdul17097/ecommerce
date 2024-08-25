683bd640c405ecc08dbf1c845e58709c7ed3ed85
abdul17097 <103925027+abdul17097@users.noreply.github.com>
2024-08-25 14:45:46 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ connectdb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   }));
