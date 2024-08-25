a5311f15b6a5064e756d371d5216df9d456ae5fa
abdulmusavir17097 <abdul.musavir@barqbytes.com>
2024-08-25 15:02:49 +0500

    update vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
-        secure: false,
+        target: "https://shopus-backend.vercel.app/",
+        changeOrigin: true,
+        secure: true,
       },
     },
   },
